a3e1a315bda4ed5b32891ed589e1077c3adeb021
Rana <rana.mohamed.97@hotmail.com>
2023-12-17 13:11:17 +0200

    feat: :recycle: Refactor mount functionality for products and cart components
    Change the mount functionality to render components in specified containers rather than directly on "dev-products" and "dev-cart" elements. This allows better encapsulation and flexibility for rendering components in different containers. It also improves code reusability and maintains a clear separation of concerns. This change addresses the need to decouple the rendering logic from specific element IDs and supports easier integration with other parts of the application.

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as mountProducts } from "products/ProductsIndex";
+import { mount as mountCart } from "cart/CartShow";
 
 console.log("Container");
+
+mountProducts(document.getElementById("products-container"));
+mountCart(document.getElementById("cart-container"));
